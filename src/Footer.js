da1bf2cabee30f863416a5cd003fca26d0bbcfaf
Andrey Zagoruiko <2500mbytes@gmail.com>
2023-01-27 11:37:05 -0800

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './App.css';
+
+function Footer() {
+  return (
+    <div className="App">
+      <div className="App-header2">
+        
+      <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Privacy
+        </a>
+        &nbsp;  &nbsp; &nbsp;
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Terms
+        </a>
+        &nbsp;  &nbsp; &nbsp;
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact
+        </a>
+
+        <div className="App-header2">
+(c) AskJoe 2023
+</div>
+      </div></div>
+  )
+}
+
+export default Footer
