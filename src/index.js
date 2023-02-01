7c609b5245141c03e9cc6275f957a62f57e1c9d4
kizey <ikizey@icloud.com>
2023-02-02 02:15:45 +0400

    add browser router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+import './index.css';
+
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Footer from './Footer';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Footer from './Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-              
-
-    <App />
-    <Footer />
+    <BrowserRouter>
+      <App />
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
