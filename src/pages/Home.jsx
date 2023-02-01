12dc16ec1f1509d823d782ab29d97b977e397a13
kizey <ikizey@icloud.com>
2023-02-02 02:30:42 +0400

    move images to it's own folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import down from '../ios.png';
-import logo from '../askjoe.png';
-import name from '../logo.png';
+import down from '../assets/images/ios.png';
+import logo from '../assets/images/askjoe.png';
+import name from '../assets/images/logo.png';
 
 const Home = () => {
   return (
     <div className='App'>
-      <header className='App-header'>
+      <content className='App-header'>
         <img src={name} width='120' className='App-logo' alt='logo' />
         <img src={logo} className='App-logo' alt='logo' />
         <img src={down} width='240' className='App-logo' alt='logo' />
-      </header>
+      </content>
     </div>
   );
 };
