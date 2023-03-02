91f3166807e50579b6a8bc49c315c1fc09f613b4
kizey <ikizey@icloud.com>
2023-03-02 21:03:57 +0400

    add route for TestChat

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Layout from './Layout';
 import Privacy from './pages/Privacy';
 import Terms from './pages/Terms';
+import TestChat from './pages/TestChat';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/terms' element={<Terms />} />
         <Route path='/privacy' element={<Privacy />} />
+        <Route path='/dev0dev1test2test3chat4chat5' element={<TestChat />} />
       </Routes>
     </Layout>
   );
